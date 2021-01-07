f733cfe7d5052e074e48a6da70f35de8ddda5717
amansharma2910 <amansharma2910@gmail.com>
2021-01-07 22:26:07 +0530

    Firebase initialization complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,28 @@ import 'screens/homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() {
-  runApp(DriveEZ());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(BackendInitialize());
+}
+
+class BackendInitialize extends StatelessWidget {
+  final Future<FirebaseApp> __initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: __initialization,
+      builder: (context, snapshot){
+        if(snapshot.hasError){
+          return ErrorScreen();
+        }
+        if(snapshot.connectionState == ConnectionState.done){
+          return DriveEZ();
+        }
+        return Loading();
+      },
+    );
+  }
 }
 
 class DriveEZ extends StatelessWidget {
@@ -19,3 +40,31 @@ class DriveEZ extends StatelessWidget {
     );
   }
 }
+
+//TODO: Make a better UI for error screen
+class ErrorScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Failed to connect",
+      home: Container(
+        child: Text("Firebase did not connect."),
+      ),
+    );
+  }
+}
+
+//TODO: Make a better UI for loading screen
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Loading",
+      home: Container(
+        child: Text("Loading..."),
+      ),
+    );
+  }
+}
+
+
